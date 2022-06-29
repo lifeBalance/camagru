d129ff76b052e3220b0f86b676640bd8314a52ce
javi <javi@blackie>
2022-06-30 01:28:24 +0300

    style 404 page

--- htdocs/app/views/404.php
+++ htdocs/app/views/404.php
@@ -1,5 +1,12 @@
 <?php require APPROOT . '/views/common/header.php'; ?>
 
-<?php echo '<h1>Woops!</h1>'; ?>
+<section class="hero is-warning is-fullheight-with-navbar">
+    <div class="hero-body">
+        <div class="">
+            <p class="title is-1">404</p>
+            <p class="subtitle">Woopsie!</p>
+        </div>
+    </div>
+</section>
 
 <?php require APPROOT . '/views/common/footer.php'; ?>
