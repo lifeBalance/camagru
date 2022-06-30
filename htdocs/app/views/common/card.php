93bc675d2a7577926f25175d4c1c8eec500d2e2f
javi <javi@blackie>
2022-07-01 01:13:05 +0300

    refactor card: comment section

--- htdocs/app/views/common/card.php
+++ htdocs/app/views/common/card.php
@@ -7,35 +7,32 @@
 
     <div class="card-content">
         <div class="media mb-1">
+            <span class="media-left">
+                <i class="fa-solid fa-heart has-text-danger"></i>
+                <?php echo $post['likes'] ?> likes
+            </span>
+            <span class="media-right">
+                <i class="fa-solid fa-comment"></i>
+                Comment
+            </span>
+        </div>
+        <div class="media mb-1">
             <div class="media-left">
                 <figure class="image is-48x48">
                     <img src="<?php echo $post['url_prof_pic'] ?>" alt="Profile image">
                 </figure>
             </div>
             <div class="media-content">
-                <p class="title is-4">John Smith</p>
-                <p class="subtitle is-6">@<?php echo $post['author_nick'] ?></p>
+                <p class="title is-6">@<?php echo $post['author_nick'] ?></p>
+                <p class="subtitle is-6"><?php echo Time::ago($post['comments'][0]['date']) ?></p>
             </div>
         </div>
 
         <div class="content" id="author-comment">
-            <div class="media mb-1">
-                <span class="media-left">
-                    <i class="fa-solid fa-heart has-text-danger"></i>
-                    <?php echo $post['likes'] ?> likes
-                </span>
-                <span class="media-right">
-                    <i class="fa-solid fa-comment"></i>
-                    Comment
-                </span>
-            </div>
+
             <!-- Comment section -->
             <div class="comments-section">
                 <!-- Author's comment -->
-                <bold class="has-text-weight-bold">
-                    <?php echo $post['author_nick'] ?>
-                </bold> <?php echo Time::ago($post['comments'][0]['date']) ?>
-                <br>
                 <?php echo $post['comments'][0]['content'] ?>
                 <!-- Hide 'Show comments' when they're being shown. -->
                 <div class="has-text-centered">
