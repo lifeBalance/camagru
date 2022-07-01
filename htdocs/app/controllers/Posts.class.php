c75d2e09ccda6e0e0861428a28e0ecaba993687b
javi <javi@blackie>
2022-07-01 19:24:46 +0300

    move images merging to helper class

--- htdocs/app/controllers/Posts.class.php
+++ htdocs/app/controllers/Posts.class.php
@@ -29,7 +29,7 @@ class Posts extends Controller
                 file_put_contents(UPLOADS_DIR . "/$name.png", file_get_contents($_POST['img']));
 
                 // Merge the user's image with the stickers
-                $this->mergeImgs(UPLOADS_DIR . "/$name.png", $_POST['stickers']);
+                Img::merge(UPLOADS_DIR . "/$name.png", $_POST['stickers']);
                 // Pic intel
                 $data = [
                     'comment'       => filter_var($_POST['comment'], FILTER_SANITIZE_FULL_SPECIAL_CHARS),
@@ -69,7 +69,7 @@ class Posts extends Controller
                 file_put_contents(UPLOADS_DIR . "/$name.png", file_get_contents($_POST['img']));
 
                 // Merge the user's image with the stickers
-                $this->mergeImgs(UPLOADS_DIR . "/$name.png", $_POST['stickers']);
+                Img::merge(UPLOADS_DIR . "/$name.png", $_POST['stickers']);
 
                 // Pic intel
                 $data = [
@@ -90,61 +90,6 @@ class Posts extends Controller
         }
     }
 
-    public function mergeImgs($user_file, $stickers)
-    {
-        // Turn the 'stickers' string (name and coordinates) into array.
-        $stickers = json_decode($stickers);
-
-        // Create  GdImage instance out of the user's image
-        $usr_gdi = imagecreatefrompng($user_file);
-        // Compute dimensions for the user's image
-        $usr_width = imagesx($usr_gdi);
-        $usr_height = imagesy($usr_gdi);
-
-        // Create a black canvas with the specified size. 
-        $canvas = imagecreatetruecolor($usr_width, $usr_height);
-        // Save the transparency information in the black canvas
-        imagesavealpha($canvas, true);
-        // Create a fully transparent background
-        $alpha = imagecolorallocatealpha($canvas, 0, 0, 0, 127);
-        // Fill the canvas with a transparent background
-        imagefill($canvas, 0, 0, $alpha);
-
-        // Copy the user's GdI object on the canvas
-        imagecopy($canvas, $usr_gdi, 0, 0, 0, 0, $usr_width, $usr_height);
-
-        // Copy the stickers to the canvas
-        foreach ($stickers as $sticker) {
-            $name   = $sticker[0];
-            $dst_x  = preg_replace("/[^0-9]/", '', $sticker[1]);
-            $dst_y  = preg_replace("/[^0-9]/", '', $sticker[2]);
-
-            // Load sticker file
-            $stickers_folder = PUBLIC_DIR . '/assets/stickers';
-            $sticker_file = $stickers_folder . "/$name.png";
-
-            // Create sticker's GdImage instance
-            $sticker_gdi = imagecreatefrompng($sticker_file);
-
-            // Copy the sticker to the canvas
-            imagecopy(
-                $canvas,
-                $sticker_gdi,
-                $dst_x,     // X coord. where we want to start pasting
-                $dst_y,     // Y coord. where we want to start pasting
-                0,          // STARTING x coord. of sticker
-                0,          // STARTING y coord. of sticker
-                128,        // Width of the sticker
-                128         // Height of the sticker
-            );
-        }
-        // Store the result as a file in the uploads folder
-        imagepng($canvas, $user_file);
-
-        // Destroy canvas once the file is written
-        imagedestroy($canvas);
-    }
-
     private function write_post_data($data)
     {
         // Add more pic intel
@@ -154,12 +99,6 @@ class Posts extends Controller
         $data['pic_id'] = $this->picModel->new($data);
         // Write intel to 'comments' table
         $this->commentModel->new($data);
-    }
-
-    public function get_extension($img)
-    {
-        $mime_type = image_type_to_mime_type(exif_imagetype($img));
-        return substr($mime_type, strpos($mime_type, "/") + 1);
     }
 
     public function index()
@@ -183,7 +122,7 @@ class Posts extends Controller
                 $tmp = [
                     'author'        => $comment_author->username,
                     'date'          => Time::ago($comment['created_at']),
-                    'profile_pic'  => $this->url_profile_pic($comment_author),
+                    'profile_pic'  => Img::url_profile_pic($comment['user_id']),
                     'content'       => $comment['comment']
                 ];
                 array_push($allComments, $tmp);
@@ -225,11 +164,4 @@ class Posts extends Controller
     }
 
     // Add function to edit a post (comment mb?)
-    private function url_profile_pic($someone)
-    {
-        if (empty($someone->profile_pic))
-            return URLROOT . "/assets/no_profile_pic.png";
-        else
-            return URLROOT . "/assets/$someone->profile_pic.png";
-    }
 }
---
+++ htdocs/app/core/Img.class.php
@@ -0,0 +1,76 @@
+<?php
+
+class Img
+{
+    static function merge($user_file, $stickers)
+    {
+        // Turn the 'stickers' string (name and coordinates) into array.
+        $stickers = json_decode($stickers);
+
+        // Create  GdImage instance out of the user's image
+        $usr_gdi = imagecreatefrompng($user_file);
+        // Compute dimensions for the user's image
+        $usr_width = imagesx($usr_gdi);
+        $usr_height = imagesy($usr_gdi);
+
+        // Create a black canvas with the specified size. 
+        $canvas = imagecreatetruecolor($usr_width, $usr_height);
+        // Save the transparency information in the black canvas
+        imagesavealpha($canvas, true);
+        // Create a fully transparent background
+        $alpha = imagecolorallocatealpha($canvas, 0, 0, 0, 127);
+        // Fill the canvas with a transparent background
+        imagefill($canvas, 0, 0, $alpha);
+
+        // Copy the user's GdI object on the canvas
+        imagecopy($canvas, $usr_gdi, 0, 0, 0, 0, $usr_width, $usr_height);
+
+        // Copy the stickers to the canvas
+        foreach ($stickers as $sticker) {
+            $name   = $sticker[0];
+            $dst_x  = preg_replace("/[^0-9]/", '', $sticker[1]);
+            $dst_y  = preg_replace("/[^0-9]/", '', $sticker[2]);
+
+            // Load sticker file
+            $stickers_folder = PUBLIC_DIR . '/assets/stickers';
+            $sticker_file = $stickers_folder . "/$name.png";
+
+            // Create sticker's GdImage instance
+            $sticker_gdi = imagecreatefrompng($sticker_file);
+
+            // Copy the sticker to the canvas
+            imagecopy(
+                $canvas,
+                $sticker_gdi,
+                $dst_x,     // X coord. where we want to start pasting
+                $dst_y,     // Y coord. where we want to start pasting
+                0,          // STARTING x coord. of sticker
+                0,          // STARTING y coord. of sticker
+                128,        // Width of the sticker
+                128         // Height of the sticker
+            );
+        }
+        // Store the result as a file in the uploads folder
+        imagepng($canvas, $user_file);
+
+        // Destroy canvas once the file is written
+        imagedestroy($canvas);
+    }
+
+    static function url_profile_pic($user_id)
+    {
+        $user = new User();
+
+        $user->findById($user_id);
+        if (empty($user->profile_pic))
+            return URLROOT . "/assets/no_profile_pic.png";
+        else
+            return URLROOT . "/assets/$user->profile_pic.png";
+    }
+
+    static function get_extension($img)
+    {
+        $mime_type = image_type_to_mime_type(exif_imagetype($img));
+        return substr($mime_type, strpos($mime_type, "/") + 1);
+    }
+}
