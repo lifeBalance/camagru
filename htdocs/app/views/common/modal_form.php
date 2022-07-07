ed000504a5f1d70c23f90d9bac533f6c4538ae93
javi <javi@blackie>
2022-07-07 13:36:52 +0300

    fix comment modal form (username and profile pic)

--- htdocs/app/views/common/modal_form.php
+++ htdocs/app/views/common/modal_form.php
@@ -1,12 +1,12 @@
 <div class="media pl-2 pt-2">
     <div class="media-left">
         <figure class="image is-48x48">
-            <img src="<?php echo $comment['profile_pic'] ?>" alt="Profile picture">
+            <img src="<?php echo Img::url_profile_pic($_SESSION['user_id']) ?>" alt="Profile picture">
         </figure>
     </div>
 
     <div class="media-content">
-        <p class="title is-6">@<?php echo $comment['author'] ?></p>
+        <p class="title is-6">@<?php echo $_SESSION['username'] ?></p>
         <p class="subtitle is-6 mb-2">Write your comment:</p>
         <div class="field">
             <input type="text" class="input is-medium mb-1" placeholder="Write a comment"></input>
