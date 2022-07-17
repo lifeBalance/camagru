819c431a47328a7c647919a04ed617ab280551e3
Javier <javi@grey.local>
2022-07-17 21:52:35 +0300

    change width of gallery page to half

--- htdocs/app/views/posts/index.php
+++ htdocs/app/views/posts/index.php
@@ -2,7 +2,7 @@
 
 <section class="section">
     <div class="columns is-centered">
-        <div class="column is-two-thirds">
+        <div class="column is-half">
             <?php require APPROOT . '/views/common/flashes.php'; ?>
 
             <?php foreach($posts as $post) {
